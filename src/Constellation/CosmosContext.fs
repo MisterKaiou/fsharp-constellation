65508a365ef654c33904b8f80424c132edc0b0a6
Kaio Rauta <kaio_soares_rauta@yahoo.com.br>
2021-09-21 22:27:07 -0300

    Added default serializer to CosmosContext.fs

--- src/Constellation/CosmosContext.fs
+++ src/Constellation/CosmosContext.fs
@@ -4,7 +4,43 @@
 module Constellation.Context
 
 open System
+open System.IO
+open System.Text.Json
 open Microsoft.Azure.Cosmos
+open System.Text.Json.Serialization
+open Constellation.TypeBuilders
+
+let defaultJsonSerializer =
+    let options = JsonSerializerOptions()
+    options.Converters.Add(JsonFSharpConverter(unionEncoding = JsonUnionEncoding.FSharpLuLike))
+    options
+
+let private deserialize<'a> (stream: Stream) =
+    try
+        if typeof<Stream>.IsAssignableFrom (typeof<'a>) then
+            (box stream) :?> 'a
+        else
+            use memoryStream = new MemoryStream()
+            stream.CopyTo(memoryStream)
+            let span = ReadOnlySpan(memoryStream.ToArray())
+            JsonSerializer.Deserialize(span, options = defaultJsonSerializer)
+
+    finally
+        stream.Dispose()
+
+let private serialize input =
+    let payload = new MemoryStream()
+    let options = JsonWriterOptions()
+    use writer = new Utf8JsonWriter(payload, options)
+
+    JsonSerializer.Serialize(writer, input, defaultJsonSerializer)
+    payload :> Stream
+
+let private defaultCosmosSerializer =
+    { new CosmosSerializer() with
+        member this.FromStream<'T>(stream: Stream) : 'T = deserialize stream
+
+        member this.ToStream<'T>(input: 'T) : Stream = serialize input }
 
 type CosmosEndpointInfo =
     { Endpoint: string
@@ -13,8 +49,8 @@ type CosmosEndpointInfo =
 type ConnectionMode =
     | ConnectionString of string
     | AccountKey of CosmosEndpointInfo
-    | Undefined 
-    
+    | Undefined
+
 /// <summary>
 ///     A wrapper around a single shared instance of CosmosClient. Providing a EF like usage for DI Containers.
 /// </summary>
@@ -23,7 +59,7 @@ type ConnectionMode =
 ///     instances, and all context instances are supposed to be a singleton, there are no safe guards against unintentional
 ///     disposing; except that a new context always checks if the client has been disposed, and creates a new one if so.
 ///     The proper behaviour can only be guaranteed if the context instances are created as singletons through the
-///     application's lifetime. 
+///     application's lifetime.
 /// </remarks>
 type CosmosContext private () =
     let mutable _databaseId = ""
@@ -33,7 +69,13 @@ type CosmosContext private () =
     static let mutable _client: CosmosClient = null
 
     member private this.setupContextClient(clientOptions: CosmosClientOptions option) =
-        let option = clientOptions |> Option.toObj
+        let getDefaultOptions () =
+            cosmosClientOptions { serializer defaultCosmosSerializer }
+
+        let option =
+            match clientOptions with
+            | Some o -> o
+            | None -> getDefaultOptions ()
 
         match _connectionMode with
         | ConnectionString s -> this.Client <- new CosmosClient(s, option)
@@ -65,18 +107,34 @@ type CosmosContext private () =
             else
                 ()
 
-    new(cosmosEndpointInfo: CosmosEndpointInfo,
-        databaseId,
-        ?clientOptions: CosmosClientOptions) as this =
+    /// <summary>
+    ///     Creates a new instance of the this class with a client configured to connect with endpoint and
+    /// account key, if not already present.
+    /// </summary>
+    /// <param name="clientOptions">
+    ///     The options with which to create the new client. If not specified, a default value with a custom
+    /// json parser will be used. (FSharp.SystemTextJson)
+    /// </param>
+    /// <param name="databaseId">The database at which this client points to</param>
+    /// <param name="cosmosEndpointInfo">This client's endpoint configuration</param>
+    new(cosmosEndpointInfo: CosmosEndpointInfo, databaseId, ?clientOptions: CosmosClientOptions) as this =
         new CosmosContext()
         then
             this.ConnectionMode <- AccountKey cosmosEndpointInfo
             this.DatabaseId <- databaseId
             this.setupContextClient clientOptions
 
-    new(connString,
-        databaseId,
-        ?clientOptions: CosmosClientOptions) as this =
+    /// <summary>
+    ///     Creates a new instance of the this class with a client configured to connect with endpoint and
+    /// account key, if not already present.
+    /// </summary>
+    /// <param name="clientOptions">
+    ///     The options with which to create the new client. If not specified, a default value with a custom
+    /// json parser will be used. (FSharp.SystemTextJson)
+    /// </param>
+    /// <param name="databaseId">The database at which this client points to</param>
+    /// <param name="connString">This client's connection string</param>
+    new(connString, databaseId, ?clientOptions: CosmosClientOptions) as this =
         new CosmosContext()
         then
             this.ConnectionMode <- ConnectionString connString
@@ -84,16 +142,18 @@ type CosmosContext private () =
             this.setupContextClient clientOptions
 
     interface IDisposable with
-        member this.Dispose() = 
+        member this.Dispose() =
             if not _disposed then
                 _disposed <- true
                 this.Client.Dispose()
                 this.ConnectionMode <- Undefined
             else
                 ()
-        
-type ConstellationContainer = 
+
+type ConstellationContainer =
     | Container of Container
-        
+
     member internal this.container =
-        this |> function Container c -> c
+        this
+        |> function
+            | Container c -> c
