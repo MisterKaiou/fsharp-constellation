69a1e4f2ed484f771b104c42c182991520228a58
Kaio Rauta <kaio_soares_rauta@yahoo.com.br>
2021-10-01 00:31:54 -0300

    Added delete methods

--- src/Constellation/TypeExtensions.fs
+++ src/Constellation/TypeExtensions.fs
@@ -2,22 +2,24 @@
 
 open Constellation.Context
 open Constellation
+open Constellation.Attributes
 open Microsoft.Azure.Cosmos
 open System.Threading
 open FSharp.Control
 
+let private getCancelToken token = token |> function Some c -> c | None -> defaultArg token (CancellationToken())
+
 type ConstellationContainer with
     member this.insertWithOptionsAsync (itemOptions: ItemRequestOptions option) (cancelToken: CancellationToken option) items =
-        let getPk this = Attributes.PartitionKeyAttributeHelpers.getPartitionKeyFromType this
-        let container = this.container
+        let getPk this = PartitionKeyAttributeHelpers.getPartitionKeyFromType this
         let options = itemOptions |> Option.toObj
-        let token = cancelToken |> function Some c -> c | None -> defaultArg cancelToken (CancellationToken())           
+        let token = cancelToken |> getCancelToken           
                 
         match items with
         | [ single ] ->
             let pk = getPk single
             
-            [ container
+            [ this.container
                 .CreateItemAsync(single, pk, options, token)
                 |> Async.AwaitTask ]
         | _ ->
@@ -26,13 +28,22 @@ type ConstellationContainer with
                    (fun curr ->
                         let pk = getPk curr
                         
-                        container
+                        this.container
                             .CreateItemAsync(curr, pk, options, token)
                             |> Async.AwaitTask
                    )
         |> AsyncSeq.ofSeqAsync
 
     member this.insertAsync items = items |> this.insertWithOptionsAsync None None
+
+    member this.deleteWithOptions (itemOptions: ItemRequestOptions option) (cancelToken: CancellationToken option) id partitionKey =
+        let options = itemOptions |> Option.toObj
+        let token = cancelToken |> getCancelToken
+        
+        this.container.DeleteItemAsync(id, partitionKey, options, token)
+        |> Async.AwaitTask
+
+    member this.delete id partitionKey = (id, partitionKey) ||> this.deleteWithOptions None None
 
 type CosmosContext with
     member this.GetContainer containerId =
